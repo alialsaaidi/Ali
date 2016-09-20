86551719a4f06615ff7a898b57a3353f5e8d93e2
alialsaaidi <alialsaaidi@aa0508-48.labs.sce.carleton.ca>
2016-09-20 12:22:02 -0400

    i add some function .

---
+++ src/AddressBook.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class AddressBook {
+	
+	
+	private ArrayList<BuddyInfo> myList;
+	
+	public AddressBook(){
+		
+		
+		ArrayList<BuddyInfo> myList = new ArrayList<BuddyInfo>();
+		
+	}
+	
+	
+	
+	public void addBuddy(BuddyInfo temp){
+	
+		myList.add(temp);
+		
+	}
+	public void removeBuddy(int index ){
+		myList.remove(index);
+		
+		
+	}
+	
+	public static void main (String[] args ){
+		
+		System.out.println("Address Book");
+	}
+
+}
