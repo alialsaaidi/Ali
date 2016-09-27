908dbe0b8e3e0bbcf9807336b7ab154612b4b97a
alialsaaidi <alialsaaidi@aa0508-48.labs.sce.carleton.ca>
2016-09-27 12:11:27 -0400

    testing

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -27,6 +27,10 @@ public class AddressBook {
 	public static void main (String[] args ){
 		
 		System.out.println("Address Book");
+		BuddyInfo buddy = new BuddyInfo ( 22 ,"Ali");
+		AddressBook addressBook = new AddressBook();
+		addressBook.addBuddy(buddy);
+		addressBook.removeBuddy(0);
 	}
 
 }
